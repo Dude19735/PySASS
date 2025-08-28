84fd848ea11a2634798d59690fac131a23c7592e
Dumeni Manatschal <dumeni.manatschal@gmail.com>
2025-08-28 18:42:37 +0200

    prepare simple module for nanobind tests

---
+++ 10_Projects/06_PySASSExt/py_sass_ext/src/CPP.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include <tuple>
+#include <map>
+#include <nanobind/nanobind.h>
+#include <iostream>
+#include "SASS_Bits.hpp"
+
+namespace nb = nanobind;
+
+namespace ACC {
+    class CPP{};
+}
